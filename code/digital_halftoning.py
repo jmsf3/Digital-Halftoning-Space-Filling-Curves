18aafe9a1e6f723f3987bd4cb6ca0eb43d47c5aa
João Marcelo <jmsf3@cin.ufpe.br>
2025-03-05 21:29:20 -0300

    Add .gitignore and refactor digital halftoning script

--- code/digital_halftoning.py
+++ code/digital_halftoning.py
@@ -8,75 +8,115 @@
 # --------------------------------------------------------------------------------------------------
 # usage examples:
 #
-# digital_halftoning.py --image data/input/araras.png --curve peano --cluster_size 4
-# digital_halftoning.py --image data/input/impa.png --curve hilbert --cluster_size 8
-# digital_halftoning.py --image data/input/araras.png --curve sierpinksi --cluster_size 6
+# digital_halftoning.py -i data/boots.jpg -o data/boots_sierpinski.jpg -t sierpinski -s 6 -d random
+# digital_halftoning.py -i data/leafs.jpg -o data/leafs_peano.jpg -t peano -s 8 -d ordered
+# digital_halftoning.py -i data/araras.png -o data/araras_peano.png -t peano -s 4 -d standard
+# digital_halftoning.py -i data/impa.png -o data/impa_hilbert.png -t hilbert -s 8 -d ordered
 # --------------------------------------------------------------------------------------------------
-# usage: digital_halftoning.py [-h] [--image image] [--curve curve] [--cluster_size cluster_size]
+# usage: digital_halftoning.py [-h] [-i image_in] [-o image_out] [-t curve_type] [-s cluster_size]
+#                              [-d distribution] [-g gamma] [-b blur] [-w weight]
 #
 # options:
-#   -h, --help                   show this help message and exit
-#   --image image                path to the input image
-#   --curve curve                type of space filling curve (hilbert, peano, sierpinski)
-#   --cluster_size cluster_size  size of the cluster for halftoning
+#   -h, --help                                    show this help message and exit
+#   -i image_in, --image_in image_in              path to the input image
+#   -o image_out, --image_out image_out           path to the output image
+#   -t curve_type, --curve_type curve_type        type of space filling curve
+#   -s cluster_size, --cluster_size cluster_size  size of the cluster for halftoning
+#   -d distribution, --distribution distribution  within-cluster intensity distribution
+#   -g gamma, --gamma gamma                       gamma value for gamma correction
+#   -b blur, --blur blur                          blur value for edge enhancement
+#   -w weight, --weight weight                    weight value for edge enhancement
 # --------------------------------------------------------------------------------------------------
 
 import argparse
 import cv2
 import numpy as np
-from space_filling_curves import peano, hilbert, sierpinski
+from space_filling_curves import space_filling_curve
 
 
-def generate_space_filling_curve(image, curve):
-    log = lambda x, b : np.log(x) / np.log(b)
+def order(image_in, curve_type):
+    """
+    Compute the order of the space-filling curve based on the input image dimensions and curve type.
 
-    if curve == 'hilbert':
-        order = np.ceil(np.log2(max(image.shape))).astype(int)
-        n = 2**order
-        space_filling_curve = [hilbert(i, order) for i in range(n * n)]
-    elif curve == 'peano':
-        order = np.ceil(log(max(image.shape), 3)).astype(int)
-        n = 3**order
-        space_filling_curve = [peano(i, order) for i in range(n * n)]
-    elif curve == 'sierpinski':
-        order = np.ceil(log(max(image.shape), 4)).astype(int)
-        n = 4**order
-        space_filling_curve = [sierpinski(i, order) for i in range(n * n)]
+    Parameters:
+    -----------
+    image_in : numpy.ndarray
+        The input image for which the curve order is to be computed.
+    curve_type : str
+        The type of space-filling curve. Must be one of ('hilbert', 'peano', 'sierpinski').
+
+    Returns:
+    --------
+    order : int
+        The computed order of the specified space-filling curve.
+    """
+
+    if curve_type == 'hilbert':
+        return np.ceil(np.log2(max(image_in.shape))).astype(int)
+    elif curve_type == 'peano':
+        return np.ceil(np.log(max(image_in.shape)) / np.log(3)).astype(int)
+    elif curve_type == 'sierpinski':
+        return np.ceil(np.log(max(image_in.shape)) / np.log(4)).astype(int)
     else:
         raise ValueError('invalid curve type, choose from (hilbert, peano, sierpinski)')
 
-    height, width = image.shape
-    space_filling_curve = [(x, y) for x, y in space_filling_curve if x < width and y < height]
 
-    return space_filling_curve
+def gamma_correction(image_in, gamma):
 
+    # TODO: Implement gamma correction
 
-def gamma_correction(image):
-
-    #TODO: Implement gamma correction
-
-    return image
+    return image_in
 
 
-def edge_enhancement(image):
+def edge_enhancement(image_in, blur, weight):
 
-    #TODO: Implement edge enhancement
+    # TODO: Implement edge enhancement
 
-    return image
+    return image_in
 
 
-def halftoning(image, curve, cluster_size):
-    halftone = np.zeros_like(image)
+def halftoning(image_in, curve_type, cluster_size, distribution):
+    """
+    Apply digital halftoning to an input image using a specified space-filling curve and
+    distribution method.
 
-    space_filling_curve = generate_space_filling_curve(image, curve)
-    n_clusters = len(space_filling_curve) // cluster_size
-    clusters = np.array_split(space_filling_curve, n_clusters)
+    Parameters:
+    -----------
+    image_in : numpy.ndarray
+        The input grayscale image to be halftoned.
+    curve_type : str
+        The type of space-filling curve to use (e.g., 'hilbert', 'peano').
+    cluster_size : int
+        The size of the clusters to divide the curve into for processing.
+    distribution : str
+        The method of distributing pixel intensities within each cluster.
+        Options are 'standard', 'ordered', or 'random'.
 
+    Returns:
+    --------
+    halftone : numpy.ndarray
+        The resulting halftoned image.
+    """
+
+    halftone = np.zeros_like(image_in)
+
+    curve = space_filling_curve(curve_type, order(halftone, curve_type))
+    curve = [(x, y) for x, y in curve if x < halftone.shape[1] and y < halftone.shape[0]]
+
+    n_clusters = len(curve) // cluster_size
+    clusters = np.array_split(curve, n_clusters)
     intensity_accumulator = 0
 
     for cluster in clusters:
+        if distribution == 'ordered':
+            cluster = sorted(cluster, key=lambda p: image_in[p[1], p[0]])
+        elif distribution == 'random':
+            np.random.shuffle(cluster)
+        elif distribution != 'standard':
+            raise ValueError('invalid distribution type, choose from (standard, ordered, random)')
+
         for x, y in cluster:
-            intensity_accumulator += image[y, x]
+            intensity_accumulator += image_in[y, x]
 
         for x, y in cluster:
             if intensity_accumulator >= 255:
@@ -92,28 +132,48 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     default = {
-        'image': 'data/input/araras.png',
-        'curve': 'hilbert',
+        'curve_type': 'hilbert',
         'cluster_size': 4,
+        'distribution': 'standard',
+        'gamma': 1.0,
+        'blur': 1.0,
+        'weight': 1.0,
     }
 
-    parser.add_argument('--image', metavar='image', type=str,
-                        default=default['image'],
+    parser.add_argument('-i', '--image_in', metavar='image_in', type=str,
                         help='path to the input image')
-    parser.add_argument('--curve', metavar='curve', type=str,
-                        default=default['curve'],
+    parser.add_argument('-o', '--image_out', metavar='image_out', type=str,
+                        help='path to the output image')
+    parser.add_argument('-t', '--curve_type', metavar='curve_type', type=str,
+                        default=default['curve_type'],
                         help='type of space filling curve (hilbert, peano, sierpinski)')
-    parser.add_argument('--cluster_size', metavar='cluster_size', type=int,
+    parser.add_argument('-s', '--cluster_size', metavar='cluster_size', type=int,
                         default=default['cluster_size'],
                         help='size of the cluster for halftoning')
+    parser.add_argument('-d', '--distribution', metavar='distribution', type=str,
+                        default=default['distribution'],
+                        help='within-cluster intensity distribution (standard, ordered, random)')
+    parser.add_argument('-g', '--gamma', metavar='gamma', type=float,
+                        default=default['gamma'],
+                        help='gamma value for gamma correction')
+    parser.add_argument('-b', '--blur', metavar='blur', type=float,
+                        default=default['blur'],
+                        help='blur value for edge enhancement')
+    parser.add_argument('-w', '--weight', metavar='weight', type=float,
+                        default=default['weight'],
+                        help='weight value for edge enhancement')
     args = parser.parse_args()
 
-    image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
-    curve = args.curve
+    image_in = cv2.imread(args.image_in, cv2.IMREAD_GRAYSCALE)
+    curve_type = args.curve_type
     cluster_size = args.cluster_size
+    distribution = args.distribution
+    gamma = args.gamma
+    blur = args.blur
+    weight = args.weight
 
-    gamma_image = gamma_correction(image)
-    edge_image = edge_enhancement(gamma_image)
-    halftone_image = halftoning(edge_image, curve, cluster_size)
+    gamma_corrected_image = gamma_correction(image_in, gamma)
+    edge_enhanced_image = edge_enhancement(gamma_corrected_image, blur, weight)
+    halftone_image = halftoning(edge_enhanced_image, curve_type, cluster_size, distribution)
 
-    cv2.imwrite(f"data/output/{curve}_{cluster_size}_{args.image.split('/')[-1]}", halftone_image)
+    cv2.imwrite(args.image_out, halftone_image)
